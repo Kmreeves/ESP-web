d9dc2e6d673c5c3ae3b182bdf9043770c8c628bb
Kaylah Reeves <kmreeves17@gmail.com>
2013-04-29 16:22:11 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ESP::Application.config.secret_token = '1b80071bd183aeeccdfa08406782786a0b344cbf37ca7134f9b36a5439ad9ac0300c0d53db094e18b793aadaa8e67b25291020881a117253903d01d8e5256e3a'
